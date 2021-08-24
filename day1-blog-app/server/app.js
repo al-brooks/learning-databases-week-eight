85ab6acde4e579e2793298cc36091b1972b1225d
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-08-24 15:03:17 -0400

    update functionality

--- day1-blog-app/server/app.js
+++ day1-blog-app/server/app.js
@@ -33,11 +33,35 @@ app.set('view engine', 'mustache');
 
 // get all posts
 app.get('/', (req, res) => {
-  db.any('SELECT title, body, post_id, user_id FROM blog_posts').then(
-    (posts) => {
-      res.render('index', { posts: posts });
-    }
-  );
+  db.any(
+    'SELECT title, body, post_id, user_id, date_updated FROM blog_posts ORDER BY date_updated DESC'
+  ).then((posts) => {
+    res.render('index', { posts: posts });
+  });
+});
+
+app.post('/post-detail', (req, res) => {
+  const post_id = parseInt(req.body.post_id);
+
+  db.one(
+    'SELECT title, body, post_id, user_id, date_updated FROM blog_posts WHERE post_id=$1',
+    [post_id]
+  ).then((post) => {
+    res.render('post-detail', { post: post });
+  });
+});
+
+app.post('/update-post', (req, res) => {
+  const title = req.body.title;
+  const body = req.body.body;
+  const post_id = parseInt(req.body.post_id);
+
+  db.none(
+    'UPDATE blog_posts SET title=$1, body=$2, date_updated=current_timestamp WHERE post_id=$3',
+    [title, body, post_id]
+  ).then(() => {
+    res.redirect('/');
+  });
 });
 
 app.post('/add-post', (req, res) => {
