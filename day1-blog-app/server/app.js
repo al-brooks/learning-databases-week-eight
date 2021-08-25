fafe9f50c6a13ae33735bb24792e0e483aeba3b6
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-08-25 10:03:07 -0400

    updated posts router

--- day1-blog-app/server/app.js
+++ day1-blog-app/server/app.js
@@ -31,59 +31,8 @@ app.engine('mustache', mustacheExpress());
 app.set('views', './templates');
 app.set('view engine', 'mustache');
 
-// get all posts
 app.get('/', (req, res) => {
-  db.any(
-    'SELECT title, body, post_id, user_id, date_updated FROM blog_posts ORDER BY date_updated DESC'
-  ).then((posts) => {
-    res.render('index', { posts: posts });
-  });
-});
-
-app.post('/post-detail', (req, res) => {
-  const post_id = parseInt(req.body.post_id);
-
-  db.one(
-    'SELECT title, body, post_id, user_id, date_updated FROM blog_posts WHERE post_id=$1',
-    [post_id]
-  ).then((post) => {
-    res.render('post-detail', { post: post });
-  });
-});
-
-app.post('/update-post', (req, res) => {
-  const title = req.body.title;
-  const body = req.body.body;
-  const post_id = parseInt(req.body.post_id);
-
-  db.none(
-    'UPDATE blog_posts SET title=$1, body=$2, date_updated=current_timestamp WHERE post_id=$3',
-    [title, body, post_id]
-  ).then(() => {
-    res.redirect('/');
-  });
-});
-
-app.post('/add-post', (req, res) => {
-  const title = req.body.title;
-  const body = req.body.body;
-  //   const is_published = true;
-  //   const user_id = 1;
-
-  db.none(
-    'INSERT INTO blog_posts(title, body, is_published, user_id) VALUES($1, $2, $3, $4)',
-    [title, body, true, 1]
-  ).then(() => {
-    res.redirect('/');
-  });
-});
-
-app.post('/delete-post', (req, res) => {
-  const post_id = parseInt(req.body.post_id);
-
-  db.none('DELETE FROM blog_posts WHERE post_id=$1', [post_id]).then(() => {
-    res.redirect('/');
-  });
+  res.render('index');
 });
 
 app.listen(PORT, () => {
--- day1-blog-app/server/routers/posts.js
+++ day1-blog-app/server/routers/posts.js
@@ -10,4 +10,50 @@ router.get('/', (req, res) => {
   });
 });
 
+router.post('/post-detail', (req, res) => {
+  const post_id = parseInt(req.body.post_id);
+
+  db.one(
+    'SELECT title, body, post_id, user_id, date_updated FROM blog_posts WHERE post_id=$1',
+    [post_id]
+  ).then((post) => {
+    res.render('post-detail', { post: post });
+  });
+});
+
+router.post('/update-post', (req, res) => {
+  const title = req.body.title;
+  const body = req.body.body;
+  const post_id = parseInt(req.body.post_id);
+
+  db.none(
+    'UPDATE blog_posts SET title=$1, body=$2, date_updated=current_timestamp WHERE post_id=$3',
+    [title, body, post_id]
+  ).then(() => {
+    res.redirect('/posts');
+  });
+});
+
+router.post('/add-post', (req, res) => {
+  const title = req.body.title;
+  const body = req.body.body;
+  //   const is_published = true;
+  //   const user_id = 1;
+
+  db.none(
+    'INSERT INTO blog_posts(title, body, is_published, user_id) VALUES($1, $2, $3, $4)',
+    [title, body, true, 1]
+  ).then(() => {
+    res.redirect('/posts');
+  });
+});
+
+router.post('/delete-post', (req, res) => {
+  const post_id = parseInt(req.body.post_id);
+
+  db.none('DELETE FROM blog_posts WHERE post_id=$1', [post_id]).then(() => {
+    res.redirect('/posts');
+  });
+});
+
 module.exports = router;
